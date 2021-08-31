a715b2264d8faa363b82932edcf30bb54d980a66
Thai Ngo <baothaingo@gmail.com>
2021-08-31 23:01:15 +0700

    add in toString method

--- src/main/java/example/models/Address.java
+++ src/main/java/example/models/Address.java
@@ -2,12 +2,14 @@ package example.models;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Embeddable;
 
 @Getter
 @Setter
 @Embeddable
+@ToString
 public class Address {
 
     private String city;
